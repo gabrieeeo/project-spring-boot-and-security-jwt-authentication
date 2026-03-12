6c9e43283c4ee96d38d974dc49baad523fcf7815
Gabriel Lima <gabrieeelima15@gmail.com>
2026-03-12 10:49:03 -0300

    first commit

---
+++ src/test/java/me/gabriel/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabriel.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
